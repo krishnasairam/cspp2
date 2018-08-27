5fa3c50a6ed0d00861f2db5941227843243fb3bc
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-27 18:10:46 +0530

    HelloWorld.java

--- cspp2-practice/m1/a3/HelloWorld.java
+++ cspp2-practice/m1/a3/HelloWorld.java
@@ -1,6 +1,19 @@
-public class HelloWorld {
-   public static void main(String[] args) {
-    // Prints "Hello, World" in the terminal window.
-    System.out.println("Hello World");
-   }
+/**
+A simple class to compile.
+*/
+
+final class HelloWorld {
+  /** main parameters.
+  */
+  private HelloWorld() {
+    /** Hello World
+    */
+  }
+  /**
+  print HelloWorld.
+  @param args of type String[]
+  */
+    public static void main(final String[] args) {
+    System.out.println("hello world");
+  }
 }
