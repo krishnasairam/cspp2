0cbd05d42bbcebcd51f1bc8e5f75312d0117e778
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-28 14:34:06 +0530

    calculateFromDegreesToFarenheit

---
+++ cspp2-assignments/m1/Assignment-4/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**.
+    * Date: 28-08-2018.
+    * Name: Sai Ram.
+    * Area of the trian.
+*/
+public final class Solution {
+    /**
+     * { var_description }.
+     */
+    static final double A = 1.8;
+    /**
+     * { var_description }.
+     */
+    static final double B = 32;
+    /**
+        * default constructor.
+    */
+    private Solution() {
+    }
+   /**
+        * main function.
+        * @param args is for commsnd line arguments.
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double degreesCelsius = scan.nextDouble();
+        calculateFromDegreesToFarenheit(degreesCelsius);
+    }
+    /**
+        * main function.
+        * @param d is temperature in celsius.
+    */
+    public static void calculateFromDegreesToFarenheit(final double d) {
+        System.out.println((d * A) + B);
+    }
+}
