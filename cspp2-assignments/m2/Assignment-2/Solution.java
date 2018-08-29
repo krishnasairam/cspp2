028fb9cb1f21e8fb3c7493ed629425803cd9cc48
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-29 09:20:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-2/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * Class for solution.
+ */
+
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * {It is the constant used in the calculation of roots}.
+     */
+    private static final double CONSTANT = 4;
+
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // Empty Constructor
+    }
+
+    /**
+     * { It is the main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+    }
+
+    /**
+     * { It is the method for finding the roots }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     *
+     */
+    public static void roots(final int a, final int b, final int c) {
+        double result = -b + Math.sqrt((b * b) - (x() * a * c));
+        result /= (2 * a);
+        double result1 = -b - Math.sqrt((b * b) - (x() * a * c));
+        result1 /= (2 * a);
+        System.out.println(result + " " + result1);
+    }
+
+    /**
+     * { Function to give the constant value }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static double x() {
+        return CONSTANT;
+    }
+}
