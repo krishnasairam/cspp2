b33ce84a06385dba3feb6613b9526e5a6d380a9d
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-30 15:48:51 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -14,37 +14,39 @@ public final class Solution {
      */
 	public static void main(final String[] args) {
 	  Scanner scan = new Scanner(System.in);
-	  int row1 = scan.nextInt();
-      int col1 = scan.nextInt();
-      scan.nextLine();
-      int[][] x1 = new int[row1][col1];
-      x1 = read(row1, col1);
-      int row2 = scan.nextInt();
-      int col2 = scan.nextInt();
-      scan.nextLine();
-      int[][] x2 = new int[row2][col2];
-      x2 = read(row2, col2);
+	  
+      int[][] x1 = read();
+      // x1 = read(row1, col1);
+      // int row2 = scan.nextInt();
+      // int col2 = scan.nextInt();
+      // scan.nextLine();
+      int[][] x2 = read();
+      // x2 = read(row2, col2);
       int[][] a = madd(x1, x2);
-      for(int i = 0; i < row1; i++ ) {
-      	for(int j =0; j < col1; j++) {
+      for(int i = 0; i < x1.length; i++ ) {
+      	for(int j =0; j < x1[0].length; j++) {
       		System.out.print(a[i][j]);
       	}
       	System.out.println();
       }
       	
   }
-    public static int[][] read(int row, int col) {
-	Scanner scan = new Scanner(System.in);
-
-      int[][] arr = new int[row][col];
-        System.out.println("enter elements");
+    public static int[][] read() {
+		Scanner scan = new Scanner(System.in);
+		int row = scan.nextInt();
+		int col = scan.nextInt();
+		scan.nextLine();
+		int[][] arr = new int[row][col];
+        // System.out.println("enter elements");
         for (int i = 0; i < row; i++) {
-          for (int j = 0; j < col; j++) {
-            arr[i][j] = scan.nextInt();
-        }scan.nextLine();
+			for (int j = 0; j < col; j++) {
+	            // arr[i][j] = scan.nextInt();
+	            System.out.println(scan.nextInt());
+	        }scan.nextLine();
         }
         return arr;
     }
+
     public static int[][] madd(int[][] m1, int[][] m2) {
     	int[][] rs = m1;
     	int row = m1.length;
