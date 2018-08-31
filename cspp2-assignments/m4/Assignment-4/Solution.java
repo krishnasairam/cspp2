38e69869e5816506d0cbb63ec3bfdc18b1245d36
krishnasairam <sairamadabala007@msitprogram.net>
2018-08-31 11:30:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -3,27 +3,35 @@ import java.util.Scanner;
  * class Solution.
  */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
 	private Solution() {
 		// private class.
 	}
 /**
- * { function_description }
+ * { function_description }.
  *
- * @param      args  The arguments
+ * @param      args  The arguments.
  */
-	public static void main(String[] args)
-	{
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
 		String reverse = reverseString(s);	
 		System.out.println(reverse);	
 	}
-	public static String reverseString(String s) {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      s     { parameter_description }.
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public static String reverseString(final String s) {
 		String str1 = "";
 		for (int i = s.length() - 1; i >= 0; i--) {
 			str1 += s.charAt(i);
 		}
 		return str1;
 	}
-   
 }
