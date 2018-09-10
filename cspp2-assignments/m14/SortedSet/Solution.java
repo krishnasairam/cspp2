e47865b6aa1d54a2c9f17f6d5131bc638134f934
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-10 16:34:00 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m14/SortedSet/Solution.java
+++ cspp2-assignments/m14/SortedSet/Solution.java
@@ -117,7 +117,7 @@ class Set {
     public int[] subSet(final int fromElement, final  int toElement) {
         int[] temp = new int[x];
         int s = 0;
-        for (int i = 0;i < size; i++) {
+        for (int i = 0;i < size - 1; i++) {
             if (set[i] >= fromElement && set[i] < toElement)
                 temp[s++] = set[i];
         } return temp;
