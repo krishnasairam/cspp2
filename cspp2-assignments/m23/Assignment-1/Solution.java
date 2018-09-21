63748fb8066f93cae86f1a684222bfa7041abcd2
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-21 15:01:40 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m23/Assignment-1/Solution.java
+++ cspp2-assignments/m23/Assignment-1/Solution.java
@@ -7,9 +7,14 @@ import java.util.ArrayList;
 import java.util.*;
 class Plagiarism {
     private ArrayList<HashMap> textlist;
+    private ArrayList<String> lss;
     private HashMap<String, Integer> frequence;
     Plagiarism() {
         textlist = new ArrayList<HashMap> ();
+        lss = new ArrayList<String> ();
+    }
+    public void loadlss(String text) {
+        lss.add(text);
     }
     public void load(String text) {
         frequence = new HashMap<String, Integer> ();
@@ -24,6 +29,54 @@ class Plagiarism {
             frequence.put(i, count);
         }
         textlist.add(frequence);
+    }
+    public void longestsubstring() {
+        List<Long> listsub = new ArrayList<Long>();
+        for (String i : lss) {
+            for (String j : lss) {
+                String sub = "";
+                for (int k = 0; k < j.length();) {
+                    for (int l = k ; l < j.length(); l++) {
+                        if (i.contains(j.substring(k, l + 1)) && sub.length() < j.substring(k, l).length()) {
+                            sub = j.substring(k, l + 1);
+                        }
+                    }
+                    k += sub.length();
+                }
+                System.out.println(sub);
+                System.out.println("------------");
+                double a = sub.length();
+                double b = i.length();
+                double c = j.length();
+                listsub.add(Math.round((a * 2 / (b + c)) * 100));
+            }
+        }
+        //System.out.println(listsub);
+        int length = lss.size();
+        int c0 = length;
+        int c1 = 1;
+        System.out.print("           ");
+        for (int m = 1; m <= length; m++) {
+            System.out.print("File");
+            System.out.print(m);
+            System.out.print(".txt");
+            System.out.print("    ");
+        }
+        System.out.println();
+        for (long x : listsub) {
+            if ((c0 % length) == 0) {
+                System.out.print("File");
+                System.out.print(c1);
+                System.out.print(".txt    ");
+            }
+            System.out.print(x);
+            System.out.print("    ");
+            c0++;
+            if ((c0 % length) == 0) {
+                System.out.println();
+                c1++;
+            }
+        }
     }
     public void bagofwords() {
         ArrayList<int[]> bag = new ArrayList<int[]> ();
@@ -50,9 +103,29 @@ class Plagiarism {
             }
         }
         int length = textlist.size();
+        int c0 = length;
+        int c1 = 1;
+        System.out.print("           ");
+        for (int m = 1; m <= length; m++) {
+            System.out.print("File");
+            System.out.print(m);
+            System.out.print(".txt");
+            System.out.print("    ");
+        }
+        System.out.println();
         for (int[] x : bag) {
+            if ((c0 % length) == 0) {
+                System.out.print("File");
+                System.out.print(c1);
+                System.out.print(".txt    ");
+            }
             System.out.print(Math.round((x[2] / (Math.sqrt(x[0]) * Math.sqrt(x[1]))) * 100));
-            System.out.println();
+            System.out.print("    ");
+            c0++;
+            if ((c0 % length) == 0) {
+                System.out.println();
+                c1++;
+            }
         }
     }
 }
@@ -61,23 +134,29 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         Plagiarism pl = new Plagiarism();
         Scanner scan = new Scanner(System.in);
-        File folder = new File(scan.next());
-        File[] listOfFiles = folder.listFiles();
-        for (File i : listOfFiles) {
-            FileReader fr = new FileReader(i);
-            BufferedReader br = new BufferedReader(fr);
-            String buffer = "";
-            String s;
-            while (((s = br.readLine()) != null)) {
-                buffer += s;
+        try {
+            File folder = new File(scan.next());
+            File[] listOfFiles = folder.listFiles();
+            for (File i : listOfFiles) {
+                FileReader fr = new FileReader(i);
+                BufferedReader br = new BufferedReader(fr);
+                String buffer = "";
+                String s;
+                while (((s = br.readLine()) != null)) {
+                    buffer += s;
+                }
+                Pattern p = Pattern.compile("[^a-z A-Z 0-9]");
+                Matcher m = p.matcher(buffer);
+                String words = m.replaceAll("").replace(".", " ").toLowerCase();
+                br.close();
+                fr.close();
+                pl.load(words);
+                pl.loadlss(words);
             }
-            Pattern p = Pattern.compile("[^a-z A-Z 0-9]");
-            Matcher m = p.matcher(buffer);
-            String words = m.replaceAll("").replace(".", " ").toLowerCase();
-            br.close();
-            fr.close();
-            pl.load(words);
+        } catch (Exception e) {
+            System.out.println("empty directory");
         }
         pl.bagofwords();
+        pl.longestsubstring();
     }
 }
